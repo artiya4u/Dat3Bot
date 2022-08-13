2705381161740003520b5fdc14fa7e4a9b518d35
Artiya Thinkumpang <artiya4u@gmail.com>
2022-08-13 08:59:36 +0700

    Remove unused file.

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -1,4 +1,6 @@
 const BASE_API_URL = "http://127.0.0.1:8000";
+// Or
+// const BASE_API_URL = "http://vision.bootlegsoft.com";
 
 function swipe(action) {
     let bnts = document.querySelectorAll('button.button');
