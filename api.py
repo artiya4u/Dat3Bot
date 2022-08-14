38a88a665bc16808b47d306a87bb60c0f280f9d8
Artiya Thinkumpang <artiya4u@gmail.com>
2022-08-14 23:31:24 +0700

    Add saving profile data.

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
+import json
+import os
 import pickle
 import shutil
 import time
+from multiprocessing import Pool
 
 import PIL
 import requests
@@ -24,7 +27,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-clf = pickle.load(open('./models/lr.sav', 'rb'))
+clf = pickle.load(open('./models/rf_n500.sav', 'rb'))
 
 
 class Image(BaseModel):
@@ -100,3 +103,135 @@ async def check(image: Image):
     except Exception as e:
         print(e)
         raise HTTPException(status_code=503, detail=str(e))
+
+
+class Profile(BaseModel):
+    name: str
+    age: int | None = None
+    verified: bool = False
+    livesIn: str
+    bio: str
+    job: str
+    school: str
+
+    passions: list[str] = []
+    lifeStyles: list[str] = []
+    photoUrls: list[str] = []
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Zwolf Cookie",
+                "age": 27,
+                "verified": True,
+                "livesIn": "Bangkok, Thailand",
+                "bio": "I’m a dog person 🐶\nIG : Cookieb2uty\n165cm",
+                "job": "Marketing",
+                "school": "PSU",
+                "passions": ["Dog lover", "Travel", "Netfilx", "Cooking", "Running"],
+                "lifeStyles": ["A", "Non-smoker", "Taurus", "Dog", "Cooking", "Bachelor"],
+                "photoUrls": [
+                    "https://images-ssl.gotinder.com/58789c82d06e40d71ed85d98/640x800_b16edea3-e424-47be-85d8-a94c2cd210b1.jpg",
+                    "https://images-ssl.gotinder.com/58789c82d06e40d71ed85d98/640x800_6019c045-25bd-4da6-81cb-6577ac2a36ea.jpg",
+                    "https://images-ssl.gotinder.com/58789c82d06e40d71ed85d98/640x800_cb8ef2cd-5db3-4e3c-adf0-7e0308a0e44e.jpg",
+                    "https://images-ssl.gotinder.com/58789c82d06e40d71ed85d98/640x800_6c1ed192-31d3-46ac-a750-786d4056cdcd.jpg",
+                    "https://images-ssl.gotinder.com/58789c82d06e40d71ed85d98/640x800_be04a41f-4f7b-45c3-9c88-a1fe1bccda61.jpg"
+                ],
+            }
+        }
+
+
+class Response(BaseModel):
+    code: str
+    result: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "code": "OK",
+                "result": "SAVED",
+            }
+        }
+
+
+def download_image_save(url):
+    """
+    Download an image from the url, convert and save it to a jpg.
+    """
+    try:
+        response = requests.get(url, stream=True)
+        file_name = f"{time.time_ns()}"
+        image_temp = f'./temp/{file_name}'
+        image_file_jpg = f'{image_temp}.jpg'
+        if url[0:url.find('?')].endswith('.jpg') or url.endswith('.jpg') or url.endswith('.jpeg'):
+            with open(image_file_jpg, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
+            return image_file_jpg, f'{file_name}.jpg'
+        else:
+            image_file_webp = f'{image_temp}.webp'
+            with open(image_file_webp, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+                img = PIL.Image.open(image_file_webp).convert('RGB')
+                img.save(image_file_jpg, 'JPEG')
+
+            os.remove(image_file_webp)
+            del response
+            return image_file_jpg, f'{file_name}.jpg'
+    except Exception as e:
+        return None, None
+
+
+def get_instagram_username(bio):
+    """
+    Get an instagram profile from bio.
+    """
+    lines = bio.split('\n')
+    for line in lines:
+        line = line.strip().lower()
+        if line.startswith('instagram'):
+            line = line.replace('instagram', '')
+            line = line.replace(':', '')
+            line = line.replace(' ', '')
+            return line
+        if line.startswith('ig'):
+            line = line.replace('ig', '')
+            line = line.replace(':', '')
+            line = line.replace(' ', '')
+            return line
+
+    return ""
+
+
+@app.post("/save", response_model=Response)
+async def save(profile: Profile):
+    """
+    Save Tinder profile data.
+    """
+    uid = f"{time.time_ns()}"  # Use nano time epoch as uid
+    user = {
+        "uuid": uid,
+        "instagram": get_instagram_username(profile.bio),
+        "name": profile.name,
+        "age": profile.age,
+        "verified": profile.verified,
+        "livesIn": profile.livesIn,
+        "bio": profile.bio,
+        "job": profile.job,
+        "school": profile.school,
+        "passions": profile.passions,
+        "lifeStyles": profile.lifeStyles,
+        "photos": [],
+    }
+    os.mkdir(f'./dataset/{uid}')
+    with Pool(4) as p:
+        image_files = p.map(download_image_save, profile.photoUrls)
+        for image_file, file_name in image_files:
+            if image_file is not None:
+                user["photos"].append(file_name)
+                shutil.move(image_file, f'./dataset/{uid}/{file_name}')
+
+    with open('./dataset.txt', 'a') as df:
+        df.write(json.dumps(user) + '\n')
+
+    return Response(code="OK", result="SAVED")
--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -60,6 +60,7 @@ function nextPhoto() { // Browse next photo for active button.
     }
 }
 
+
 function requestCheckImage(profileImageURL) {
     let headers = new Headers();
     headers.append("Content-Type", "application/json");
@@ -76,15 +77,37 @@ function requestCheckImage(profileImageURL) {
     return fetch(BASE_API_URL + '/check', requestOptions).then(response => response.json());
 }
 
+function saveProfile(profile) {
+    let headers = new Headers();
+    headers.append("Content-Type", "application/json");
+    let payload = {
+        "name": profile.name ?? '',
+        "age": profile.age ?? null,
+        "verified": profile.verified ?? false,
+        "livesIn": profile.livesIn ?? '',
+        "bio": profile.bio ?? '',
+        "job": profile.job ?? '',
+        "school": profile.school ?? '',
+        "passions": profile.passions ?? [],
+        "lifeStyles": profile.lifeStyles ?? [],
+        "photoUrls": profile.photoUrls ?? [],
+    }
+    let raw = JSON.stringify(payload);
+    let requestOptions = {
+        method: 'POST', headers: headers, body: raw, redirect: 'follow'
+    };
 
-async function findPhotos() {
+    return fetch(BASE_API_URL + '/save', requestOptions).then(response => response.json());
+}
+
+
+function findPhotos() {
     let photos = [];
     try {
         let images = document.querySelectorAll('div.StretchedBox[role="img"]');
         for (const imageNode of images) {
             let hide = imageNode.parentNode.parentNode.parentNode.parentNode.getAttribute('aria-hidden');
             if (hide === 'false') { // Displaying profile's image
-                // Do something with the image
                 let imgUrl = imageNode.style.backgroundImage;
                 imgUrl = imgUrl.substring(imgUrl.indexOf("\"") + 1, imgUrl.lastIndexOf("\""));
                 photos.push(imgUrl);
@@ -93,6 +116,76 @@ async function findPhotos() {
         return photos;
     } catch (e) {
         return photos;
+    }
+}
+
+function getValueBySelector(parent, selector) {
+    let valueNode = parent.querySelector(selector)
+    if (valueNode !== null) {
+        return valueNode.innerText;
+    }
+    return null;
+}
+
+function findInfo(profile) {
+    try {
+        // let nameNode = document.querySelectorAll('span[itemprop="name"]')[1];
+        let names = document.querySelectorAll('span[itemprop="name"]');
+        for (const nameNode of names) {
+            let parent = nameNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+            let hide = parent.getAttribute('aria-hidden');
+            if (hide === 'false') { // Active profile
+                profile.name = nameNode.innerText;
+
+                let age = getValueBySelector(parent, 'span[itemprop="age"]');
+                if (age !== null) {
+                    profile.age = age;
+                }
+
+                let livingIn = getValueBySelector(parent, 'div[itemprop="homeLocation"]');
+                if (livingIn != null) {
+                    profile.livingIn = livingIn.replace("Lives in ", "");
+                }
+
+                let school = getValueBySelector(parent, 'div[itemprop="affiliation"]');
+                if (school != null) {
+                    profile.school = school.replace("Also went to ", "");
+                }
+
+                let job = getValueBySelector(parent, 'div[itemprop="jobTitle"]');
+                if (job != null) {
+                    profile.job = job;
+                }
+
+                let bio = getValueBySelector(parent, '.BreakWord');
+                if (bio != null && bio !== '') {
+                    profile.bio = bio;
+                }
+
+                let titleNode = nameNode.parentNode.parentNode
+                let verified = titleNode.querySelector('svg')
+                if (verified != null) {
+                    profile.verified = true;
+                }
+
+                let bubbles = parent.querySelectorAll('div[tabindex="-1"]')
+                if (bubbles.length > 0) {
+                    let lists = [];
+                    for (const bubble of bubbles) {
+                        lists.push(bubble.innerText);
+                    }
+                    if (profile.passions === undefined) {
+                        profile.passions = lists;
+                    } else if (profile.lifeStyles === undefined) {
+                        if (lists.toString() !== profile.passions.toString()) {
+                            profile.lifeStyles = lists;
+                        }
+                    }
+                }
+            }
+        }
+    } catch (e) {
+        console.log(e);
     }
 }
 
@@ -105,32 +198,40 @@ async function swipeLoop() {
             location.reload();
         } else {
             try {
+                let profile = {};
+                findInfo(profile);
+
                 const allPhotos = new Set();
+
                 for (let i = 0; i < 8; i++) { // Browse photos
-                    let photos = await findPhotos();
+                    findInfo(profile);
+                    let photos = findPhotos();
                     for (const photo of photos) {
                         allPhotos.add(photo);
                     }
-                    if ((i - allPhotos.size) >= 0) { // No more photos
+                    if (i > 3 && (i - allPhotos.size) >= 0) { // No more photos
                         break;
                     }
-                    await sleep(0.3 + Math.random());
+                    await sleep(0.5);
                     nextPhoto();
                 }
-                let action = 'pass';
+                profile.photoUrls = Array.from(allPhotos);
+                console.log(profile);
                 console.log('Photos Count', allPhotos.size);
-                for (const photo of allPhotos) {
-                    let result = await requestCheckImage(photo);
-                    if (result.code === 'OK') {
-                        if (result.result === 'HOT') {
-                            console.log('😍 HOT!', photo);
-                            action = 'like';
-                            break;
+                if (allPhotos.size > 0) { // Must have photos to swipe
+                    await saveProfile(profile);
+                    let action = 'pass';
+                    for (const photo of allPhotos) {
+                        let result = await requestCheckImage(photo);
+                        if (result.code === 'OK') {
+                            if (result.result === 'HOT') {
+                                console.log('😍 HOT!', photo);
+                                action = 'like';
+                                break;
+                            }
                         }
                     }
-                }
-                if (allPhotos.size > 0) { // Must have photos to swipe
-                    swipe(action);
+                    swipe('pass');
                 }
             } catch (e) {
                 console.log(e);
